e31d5f0af5e40e74f06e4e961f45324d250c5c72
Medet Kalpebaev <medetkalpebaev@Medets-MacBook-Air.local>
2021-04-21 01:19:14 +0600

    End of sections

--- API/Entites/AppUser.cs
+++ API/Entites/AppUser.cs
@@ -1,8 +1,13 @@
+
 namespace API.Entites
 {
-    public class AppUser
+    public class AppUser  
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSold { get; set; }
     }
 }
