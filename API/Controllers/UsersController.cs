2bcd4191b0be2d2906b63fb52032c35e85d25068
Medet Kalpebaev <medetkalpebaev@Medets-MacBook-Air.local>
2021-04-24 18:22:36 +0600

    End of users serialize

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,31 +8,31 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
 
-    [ApiController]
-    [Route("api/[Controller]")]
-    public class UsersController : ControllerBase
+      
+{
+        [Authorize]
+    public class UsersController : BaseApiController
     {
+
+        
         private readonly DataContext _context;
+        
         public UsersController(DataContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() 
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
-
         }
-        [Authorize]
+
         [HttpGet("{id}")]
-        public async  Task<ActionResult<AppUser>> GetUser(int id) 
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
- 
         }
     }
 }
